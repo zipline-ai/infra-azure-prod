513ef18b775255ec74d89eebdd967bcbd6e02ae8
Cristian Figueroa <cristian@zipline.ai>
2026-03-12 17:04:12 -0700

    feat: eval behind ui ingress

--- zipline-core/helm.tf
+++ zipline-core/helm.tf
@@ -76,12 +76,8 @@ resource "helm_release" "zipline_orchestration" {
       orchestration_hub_static_ip       = azurerm_public_ip.hub_ingress.ip_address
       orchestration_ui_static_ip_name   = azurerm_public_ip.ui_ingress.name
       orchestration_ui_static_ip        = azurerm_public_ip.ui_ingress.ip_address
-      orchestration_eval_static_ip_name = azurerm_public_ip.eval_ingress.name
-      orchestration_eval_static_ip      = azurerm_public_ip.eval_ingress.ip_address
-
       hub_dns_name       = "${var.hub_domain}"
       ui_dns_name        = "${var.ui_domain}"
-      eval_dns_name      = "${var.eval_domain}"
       cert_manager_email = var.admin_email
 
       node_resource_group = var.aks_node_resource_group
@@ -145,13 +141,6 @@ resource "azurerm_public_ip" "ui_ingress" {
   sku                 = "Standard"
 }
 
-resource "azurerm_public_ip" "eval_ingress" {
-  name                = "${var.customer_name}-zipline-eval-pip"
-  resource_group_name = var.aks_node_resource_group
-  location            = var.location
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
 
 # ------------------------------------------------------------------
 # AUTHENTICATION SETUP (OAuth2 Proxy + Azure AD)
@@ -389,9 +378,6 @@ output "ui_address" {
   value = azurerm_public_ip.ui_ingress.ip_address
 }
 
-output "eval_address" {
-  value = azurerm_public_ip.eval_ingress.ip_address
-}
 
 output "dns_setup_instructions" {
   description = "Instructions for configuring DNS records"
@@ -413,10 +399,6 @@ RECORD 2 (UI):
   - Type:       A
   - Value:      ${azurerm_public_ip.ui_ingress.ip_address}
 
-RECORD 3 (Eval):
-  - Host/Name:  ${var.eval_domain}
-  - Type:       A
-  - Value:      ${azurerm_public_ip.eval_ingress.ip_address}
 
 --------------------------------------------------------------------------------
 Once configured, please allow a few minutes for DNS propagation.
