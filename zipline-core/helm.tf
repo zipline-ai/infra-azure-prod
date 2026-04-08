2f439dc7bc6d8cb10f25bc63c2874cab711a8350
chewy-zlai <chewys@zipline.ai>
2026-04-08 13:44:40 -0700

    Mark secrets sensitive and a few fixes

--- zipline-core/helm.tf
+++ zipline-core/helm.tf
@@ -86,11 +86,9 @@ resource "helm_release" "zipline_orchestration" {
       cert_manager_email = var.admin_email
 
       node_resource_group = var.aks_node_resource_group
-
-      enable_oauth         = var.enable_oauth
       zipline_auth_enabled = var.zipline_auth_enabled
       zipline_auth_url     = var.ui_domain != "" ? "https://${var.ui_domain}" : "http://zipline-orchestration-ui.zipline-system.svc.cluster.local:3000"
-      zipline_auth_jwksUrl = "https://${var.ui_domain != "" ? var.ui_domain : "http://zipline-orchestration-ui.zipline-system.svc.cluster.local:3000"}/api/auth/jwks"
+      zipline_auth_jwksUrl = var.ui_domain != "" ? "https://${var.ui_domain}/api/auth/jwks" : "http://zipline-orchestration-ui.zipline-system.svc.cluster.local:3000/api/auth/jwks"
       google_oauth_client_id = var.google_oauth_client_id
       github_oauth_client_id = var.github_oauth_client_id
       microsoft_entra_tenant_id = var.microsoft_entra_tenant_id
